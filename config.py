7385aa9877841dab8571ca22f274f901c67d31a2
Hamslave <ka.haamed@gmail.com>
2025-04-05 16:55:17 +0330

    Update2 config.py

--- config.py
+++ config.py
@@ -9,3 +9,12 @@ if not TOKEN:
     raise ValueError("TOKEN is not set in the environment variables")
 if not SHEET_ID:
     raise ValueError("SHEET_ID is not set in the environment variables")
+import os
+
+# دریافت محتوای کلید گوگل از متغیر محیطی
+google_credentials = os.environ.get("CREDENTIALS_FILE")
+if google_credentials:
+    # اگر در متغیر محیطی خط‌های جدید به صورت "\\n" ذخیره شده‌اند، آن‌ها را به "\n" تبدیل می‌کنیم.
+    google_credentials = google_credentials.replace('\\n', '\n')
+    with open("credentials.json", "w") as f:
+        f.write(google_credentials)
